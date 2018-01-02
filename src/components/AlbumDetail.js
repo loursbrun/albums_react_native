09ad19a900490b7e3b52430915df60b806c5655a
fabien.brun <fabienbrun@hotmail.fr>
2018-01-02 19:21:54 +0100

    add albumDetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = () => { 
+return (
+  <View>
+      <Text>Coco</Text>
+  </View>
+ );
+};
+
+
+export default AlbumDetail;
--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 class AlbumList extends Component {
@@ -12,7 +13,9 @@ componentWillMount() {
 }
 
 renderAlbums() {
-    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+    return this.state.albums.map(album => 
+    <AlbumDetail key={album.title} />
+    );
 }
 
     render() {
